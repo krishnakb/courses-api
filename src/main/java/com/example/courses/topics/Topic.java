ccb3d6f690b8686d32f340f82a9321e1e7e5b377
krishnakb <krishnakb@live.com>
2018-11-12 17:35:07 +0100

    Adding Apache Derby dependency to read and write from/to database

--- src/main/java/com/example/courses/topics/Topic.java
+++ src/main/java/com/example/courses/topics/Topic.java
@@ -1,7 +1,12 @@
 package com.example.courses.topics;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
 public class Topic {
 
+	@Id
 	private String id;
 	private String name;
 	private String description;

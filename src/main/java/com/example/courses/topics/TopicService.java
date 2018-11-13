c5542b3d3c8c6869e68cf6d42e251b1a87d6e263
krishnakb <krishnakb@live.com>
2018-11-13 07:13:05 +0100

    Implementing the use of derby instead of a hardcoded list

--- src/main/java/com/example/courses/topics/TopicService.java
+++ src/main/java/com/example/courses/topics/TopicService.java
@@ -3,39 +3,39 @@ package com.example.courses.topics;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class TopicService {
-	
-	private List<Topic> topics = new ArrayList<Topic>(Arrays.asList(
-			new Topic("1", "java", "Basic Java"),
-			new Topic("2", "spring", "Java with Spring"),
-			new Topic("3", "spring MVC", "MVC Deep Dive")
-			));
 
-	public List<Topic> getAllTopics(){
+	@Autowired
+	TopicRepository topicRepository;
+
+//	private List<Topic> topics = new ArrayList<Topic>(Arrays.asList(new Topic("1", "java", "Basic Java"),
+//			new Topic("2", "spring", "Java with Spring"), new Topic("3", "spring MVC", "MVC Deep Dive")));
+
+	public List<Topic> getAllTopics() {
+		List<Topic> topics = new ArrayList<>();
+		topicRepository.findAll().forEach(topics::add);
 		return topics;
 	}
-	
-	public Topic getTopic(String id) {
-		return topics.stream().filter(t -> t.getId().equalsIgnoreCase(id)).findFirst().get();
+
+	public Optional<Topic> getTopic(String id) {
+		return topicRepository.findById(id);
 	}
-	
+
 	public void addTopic(Topic topic) {
-		topics.add(topic);
+		topicRepository.save(topic);
 	}
-	
+
 	public void updateTopic(String id, Topic topic) {
-		for(int i=0; i<topics.size(); i++) {
-			if(topics.get(i).getId().equalsIgnoreCase(id)) {
-				topics.set(i, topic);
-			}
-		}
+		topicRepository.save(topic);
 	}
 
 	public void deleteTopic(String id) {
-		topics.removeIf(t -> t.getId().equalsIgnoreCase(id));
+		topicRepository.deleteById(id);;
 	}
 }
